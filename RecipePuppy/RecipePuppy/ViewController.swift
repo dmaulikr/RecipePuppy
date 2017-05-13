a5c7baf1b1840d871352f6cd8bb5841dc886c22f
Chris Allison <chris@quimarche.co.uk>
2017-05-13 18:28:10 +0100

    Remove cruft.

--- RecipePuppy/RecipePuppy/ViewController.swift
+++ RecipePuppy/RecipePuppy/ViewController.swift
@@ -1,23 +1,4 @@
-//
-//  ViewController.swift
-//  RecipePuppy
-//
-//  Created by Chris Allison on 13/05/2017.
-//  Copyright © 2017 Chris Allison. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
