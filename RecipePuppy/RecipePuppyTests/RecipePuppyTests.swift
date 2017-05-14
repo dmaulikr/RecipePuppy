ee4f7d5d99c88c0b06c1212d136c83b83fa2e2ef
Chris Allison <chris@quimarche.co.uk>
2017-05-14 15:53:56 +0100

    Add unit tests.

--- RecipePuppy/RecipePuppyTests/RecipePuppyTests.swift
+++ RecipePuppy/RecipePuppyTests/RecipePuppyTests.swift
@@ -1,5 +1,14 @@
 import XCTest
 
 @testable import RecipePuppy
+
 class RecipePuppyTests: XCTestCase {
+
+    func testBundleDisplayName() {
+        XCTAssert(Bundle.displayName() == "RecipePuppy")
+    }
+
+    func testStringTrim() {
+        XCTAssert(" 123 456 789 ".trim() == "123 456 789")
+    }
 }
